dea81c8248278d421cc916fbc476adf2cf7322a5
Daniel <danieruone@gmail.com>
2021-06-23 15:34:43 -0500

    class based server

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-// express server
-const express = require("express");
-const app = express();
-// socket server
-const server = require("http").createServer(app);
-//socket configuration server
-const io = require("socket.io")(server);
+const Server = require("./models/server");
+const server = new Server();
 
-app.use(express.static(__dirname + "/public"));
-
-io.on("connection", (socket) => {
-  console.log(socket.id);
-
-  socket.emit("welcome-message", {
-    message: `welcome to the server ${socket.id}`,
-    date: new Date(),
-  });
-
-  socket.on("client-message", ({ message, date, color }) => {
-    io.emit("client-message", {
-      message,
-      date,
-      color,
-    });
-    console.log(`ìd: ${socket.id}, message: ${message}`);
-  });
-});
-
-const port = process.env.PORT || 8080;
-
-server.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+server.execute();
