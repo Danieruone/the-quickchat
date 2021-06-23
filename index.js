c25e5ab1bd6f5a3c19b0b3a1e17947e5df6d2c79
Daniel <danieruone@gmail.com>
2021-06-22 21:23:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// express server
+const express = require('express');
+const app = express();
+// socket server
+const server = require('http').createServer(app);
+//socket configuration server
+const io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.emit('welcome-message', {
+        message: `welcome to the server ${socket.id}`,
+        date: new Date()
+    })
+
+    socket.on('client-message', ({message, date, color})=>{
+        io.emit('client-message', {
+            message, 
+            date,
+            color
+        })
+        console.log(`ìd: ${socket.id}, message: ${message}`);
+    })
+});
+
+server.listen(8080, ()=>{
+    console.log("Server running on port: 8080");
+});
